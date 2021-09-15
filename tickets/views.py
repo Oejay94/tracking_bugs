752af008e636b8637cb358bc428eee1c402defe5
Joey Brown <joseph.d.bronw94@gmail.com>
2021-09-15 12:35:15 -0400

    created views for creating a ticket and displaying a single ticket

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.views import View
 
-# Create your views here.
+from .models import Ticket
+from .forms import TicketForm
+
+class CreateTicketPage(View):
+    def get(request):
+        html = 'creat_ticket.html'
+        form = TicketForm()
+        return render(request, html, {'form':form})
+    
+    def post(request):
+        form = TicketForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            Ticket.objects.create(
+                title=data['title'],
+                body=data['body'],
+                created_by=request.user
+            )
+            return HttpResponseRedirect(reverse('project_page'))
+
+
+def ticket_detail_page(request, ticket_id):
+    html = 'ticket.html'
+    ticket = Ticket.objects.get(id=ticket_id)
+    return render(request, html, {'ticket':ticket})
