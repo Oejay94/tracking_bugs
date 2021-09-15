35ae3076d266f8b3eb38e283450f4dfafa39c829
Joey Brown <joseph.d.bronw94@gmail.com>
2021-09-15 12:59:39 -0400

    added forms to update and complete tickets

--- tickets/forms.py
+++ tickets/forms.py
@@ -2,6 +2,9 @@ from django import forms
 
 from .models import Ticket
 
+from bug_tracker_users.models import BugTrackingUser
+
+#Form to create a new ticket. It inherets the specified fields from the Ticket Model
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
@@ -9,3 +12,15 @@ class TicketForm(forms.ModelForm):
             'title',
             'body'
         ]
+
+#Form to update a ticket and assign a user to it
+class UpdateTicketForm(forms.Form):
+    title = forms.CharField(max_length=40)
+    body = forms.CharField(widget=forms.Textarea())
+    assigned_to = forms.ModelChoiceField(queryset=BugTrackingUser.objects.all())
+
+#Form to 'complete' a ticket and assign a user to the completed field
+class CompleteTicketForm(forms.Form):
+    title = forms.CharField(max_length=40)
+    body = forms.CharField(widget=forms.Textarea())
+    completed_by = forms.ModelChoiceField(queryset=BugTrackingUser.objects.all())
--- tickets/views.py
+++ tickets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.views import View
 
 from .models import Ticket
-from .forms import TicketForm
+from .forms import TicketForm, UpdateTicketForm, CompleteTicketForm
 
 class CreateTicketPage(View):
     def get(request):
@@ -19,6 +19,47 @@ class CreateTicketPage(View):
                 title=data['title'],
                 body=data['body'],
                 created_by=request.user
+            )
+            return HttpResponseRedirect(reverse('project_page'))
+
+
+class UpdateTicketPage(View):
+    def get(request):
+        html = 'update_ticket.html'
+        form = UpdateTicketForm()
+        return render(request, html, {'form':form})
+    
+    def post(request, ticket_id):
+        form = UpdateTicketForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            Ticket.objects.filter(id=ticket_id).update(
+                title=data['title'],
+                body=data['body'],
+                assigned_to=data['assigned_to'],
+                status='In Progress'
+            )
+            return HttpResponseRedirect(reverse('project_page'))
+
+
+class CompleteTicketPage(View):
+    def get(request):
+        html = 'complete_ticket.html'
+        form = CompleteTicketForm()
+        return render(request, html, {'form':form})
+    
+    def post(request, ticket_id):
+        form = CompleteTicketForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            Ticket.objects.filter(id=ticket_id).update(
+                assigned_to=None,
+                completed_by=request.user,
+                status='Done',
+                title=data['title'],
+                body=data['body']
             )
             return HttpResponseRedirect(reverse('project_page'))
 
