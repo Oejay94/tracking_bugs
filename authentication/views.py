428c81af2e6ad73b41bf12688c8a99641258e308
Joey Brown <joseph.d.bronw94@gmail.com>
2021-09-15 15:39:49 -0400

    added views to create an account, login, and logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
 
-# Create your views here.
+from bug_tracker_users.models import BugTrackingUser
+
+from .forms import CreateAccountForm, LoginForm
+
+class CreateAccountPage(View):
+    def get(self, request):
+        html = 'auth/create_account_page.html'
+        form = CreateAccountForm()
+        return render(request, html, {'form':form})
+    
+    def post(self, request):
+        form = CreateAccountForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            new_user = BugTrackingUser.objects.create_user(
+                username=data['username'],
+                password=data['password1'],
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                role=data['role']
+            )
+            login(request, new_user)
+            return HttpResponseRedirect(reverse('user_page'))
+    
+
+class LoginPage(View):
+    def get(self, request):
+        html = 'auth/login.html'
+        form = LoginForm()
+        return render(request, html, {'form':form})
+    
+    def post(self, request):
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                username=data['username'],
+                password=data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('user_page'))
+
+
+def logout_button(request):
+    logout(request)
+    return HttpResponseRedirect(request.GET.get('next', '/login/'))
