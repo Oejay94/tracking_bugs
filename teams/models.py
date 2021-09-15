bc0bb10fdae5ab310e600fa14c7106f304a4c2a0
Joey Brown <joseph.d.bronw94@gmail.com>
2021-09-15 09:21:33 -0400

    added form to create team. Also renamed team roles in teams.models and added team field to project allowing admin to assign a team to a project.

--- teams/models.py
+++ teams/models.py
@@ -10,16 +10,16 @@ class Team(models.Model):
     team_bio = models.TextField(blank=True, null=True)
 
     #Roles in team
-    project_manager = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='project_manager', blank=True, null=True)
-    product_owner = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='product_owner', blank=True, null=True)
-    team_lead = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='team_lead', blank=True, null=True)
-    full_stack_dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='full_stack_dev', blank=True, null=True)
-    front_end_dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='front_end_dev', blank=True, null=True)
-    back_end_dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='back_end_dev', blank=True, null=True)
-    qa_lead = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='qa_lead', blank=True, null=True)
-    qa_dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='qa_dev', blank=True, null=True)
-    ui_designer = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='ui_designer', blank=True, null=True)
-    ux_designer = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='ux_designer', blank=True, null=True)
+    project_Manager = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='project_manager', blank=True, null=True)
+    product_Owner = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='product_owner', blank=True, null=True)
+    team_Lead = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='team_lead', blank=True, null=True)
+    full_Stack_Dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='full_stack_dev', blank=True, null=True)
+    front_End_Dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='front_end_dev', blank=True, null=True)
+    back_End_Dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='back_end_dev', blank=True, null=True)
+    qA_Lead = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='qa_lead', blank=True, null=True)
+    qA_Dev = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='qa_dev', blank=True, null=True)
+    uI_designer = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='ui_designer', blank=True, null=True)
+    uX_Designer = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='ux_designer', blank=True, null=True)
 
     def __str__(self):
         return self.team_name
