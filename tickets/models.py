1436538de757544034c56f1694adcb86a5d1ba40
Oejay94 <joseph.d.brown94@gmail.com>
2021-09-14 22:55:09 -0400

    created Project model and added project field to Ticket model. This will allow us to automatically add the ticket to the desired project upon creation.

--- tickets/models.py
+++ tickets/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 
 from bug_tracker_users.models import BugTrackingUser
+from projects.models import Project
 
 class Ticket(models.Model):
     #Variable to remove seconds and miliseconds from datetime.now()
@@ -23,6 +24,7 @@ class Ticket(models.Model):
     status = models.CharField(max_length=15, choices=TICKET_STATUS_CHOICES, default=New, null=True)
     body = models.TextField()
     date_created = models.DateTimeField(default=dt)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project', blank=True, null=True)
     created_by = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='created_by', blank=True, null=True)
     assigned_to= models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='assigned_to', blank=True, null=True)
     completed_by = models.ForeignKey(BugTrackingUser, on_delete=models.CASCADE, related_name='completed_by', blank=True, null=True)
